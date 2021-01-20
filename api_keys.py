c98071c3e168efcf6728007e822b43da604e0e69
matkuhlm <68480067+matkuhlm@users.noreply.github.com>
2021-01-19 20:39:51 -0500

    Setup and sticking point

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e5a3708ad1eeca01a0de4bde56127e37"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC0YMceKU9OjQQsMoHxhLg4LKondeAkJC4"
